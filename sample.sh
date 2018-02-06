ec04434cfe7471e7be14f01cc6527d1095240f19
BradleyA <allen.bradley@ymail.com>
2018-02-06 17:08:09 -0600

    commit 1.3 release

--- sample.sh
+++ sample.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #	sample.sh	0.02.33	2018-02-05_22:22:57_CST uadmin six-rpi3b.cptx86.com 1.2-29-g01daba2
 #	Closes #1
-#	sample.sh	0.01.29	2018-02-05_20:51:50_CST uadmin six-rpi3b.cptx86.com 1.2-25-g0d6e47a
+#	sample.sh	1.2.29	2018-02-05_20:51:50_CST uadmin six-rpi3b.cptx86.com 1.2-25-g0d6e47a
 #	Inital commit
 
