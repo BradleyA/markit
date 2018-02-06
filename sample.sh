8508643d5479c0417f16619b386e20b79a57051c
BradleyA <allen.bradley@ymail.com>
2018-02-05 20:02:01 -0600

    corrected logic in case statement

--- sample.sh
+++ sample.sh
@@ -1,3 +1,5 @@
 #
+#	sample.sh	1.01.16	2018-02-05_20:00:55_CST uadmin six-rpi3b.cptx86.com 1.01
+#	corrected logic in case statement
 #	sample.sh	1.01.15	2018-02-05_19:57:39_CST uadmin six-rpi3b.cptx86.com 2.0
 #	inital commit
