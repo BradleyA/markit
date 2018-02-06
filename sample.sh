23a9091b576ab178faf69b28181d778ac6e9900f
BradleyA <allen.bradley@ymail.com>
2018-02-05 20:03:27 -0600

    $ ./markit sample.sh

--- sample.sh
+++ sample.sh
@@ -1,4 +1,6 @@
 #
+#	sample.sh	1.01.17	2018-02-05_20:03:17_CST uadmin six-rpi3b.cptx86.com 1.01-1-g8508643
+#	$ ./markit sample.sh
 #	sample.sh	1.01.16	2018-02-05_20:00:55_CST uadmin six-rpi3b.cptx86.com 1.01
 #	corrected logic in case statement
 #	sample.sh	1.01.15	2018-02-05_19:57:39_CST uadmin six-rpi3b.cptx86.com 2.0
