513aa7b05a0b5d7b99438cbd66163423c4e1b883
BradleyA <allen.bradley@ymail.com>
2018-02-05 20:04:14 -0600

    this is a test

--- sample.sh
+++ sample.sh
@@ -1,4 +1,6 @@
 #
+#	sample.sh	2.01.18	2018-02-05_20:04:03_CST uadmin six-rpi3b.cptx86.com 1.01-2-g23a9091
+#	this is a test
 #	sample.sh	1.01.17	2018-02-05_20:03:17_CST uadmin six-rpi3b.cptx86.com 1.01-1-g8508643
 #	$ ./markit sample.sh
 #	sample.sh	1.01.16	2018-02-05_20:00:55_CST uadmin six-rpi3b.cptx86.com 1.01
