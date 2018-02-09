77936db7fb0010f1ae77618fdd32e9f42c69c795
BradleyA <allen.bradley@ymail.com>
2018-02-09 16:59:10 -0600

    create output for README #6

--- sample.sh
+++ sample.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+#	sample.sh	3.1.55	2018-02-09_16:58:41_CST uadmin six-rpi3b.cptx86.com 3.1-12-g74fb4b0 
+#	create output for README #6 
 #	sample.sh	3.1.48	2018-02-07_22:39:36_CST uadmin six-rpi3b.cptx86.com 3.1-5-ge772a21 
-#	Closes #2 
-#			1.6
-#			1.5
+#	Add an argument $1 to support -u for untracked files, Closes #2 
 #	sample.sh	1.4.33	2018-02-05_22:22:57_CST uadmin six-rpi3b.cptx86.com 1.2-29-g01daba2
-#	Closes #1
+#	show current release when entering new release number, Closes #1
 #	sample.sh	1.2.29	2018-02-05_20:51:50_CST uadmin six-rpi3b.cptx86.com 1.2-25-g0d6e47a 
 #	Inital commit
 
