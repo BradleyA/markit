95418e165108c880afe799b135a3a11d8e01c786
BradleyA <allen.bradley@ymail.com>
2018-02-12 00:18:27 -0600

    testing x x.MK x.h x.sh for #5

--- sample.h
+++ sample.h
@@ -1,4 +1,9 @@
 ################################33333
+/* 	sample.h	3.2.84	2018-02-12_00:18:10_CST uadmin six-rpi3b.cptx86.com 3.2-3-g270e96e  */
+/* 	testing x x.MK x.h x.sh for #5  */
+
+
+
 /* 	sample.h	3.2.83	2018-02-12_00:12:34_CST uadmin six-rpi3b.cptx86.com 3.2-2-g6d8266e  */
 /* 	testing x x.MK x.h x.sh for #5  */
 
