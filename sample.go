c6b50ed2a084895d5cca5c84dbea0bf83b1155cd
BradleyA <allen.bradley@ymail.com>
2018-02-12 14:58:13 -0600

    beta 3.3 testing sample.go sample.c for #5

--- sample.go
+++ sample.go
@@ -1,4 +1,7 @@
 #########################3333
+// 	sample.go	3.3.88	2018-02-12_14:56:52_CST uadmin six-rpi3b.cptx86.com 3.3-2-g71c8992 
+// 	beta 3.3 testing sample.go sample.c for #5 
+
 // 	sample.go	3.3.86	2018-02-12_14:31:08_CST uadmin six-rpi3b.cptx86.com 3.3 
 // 	beta 3.3 testing sample.go sample.c sample.xml sample.scala sample.htm sample.rb 
 
