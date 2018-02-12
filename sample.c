714d00768899540f6c606bdaf66b462541120ea0
BradleyA <allen.bradley@ymail.com>
2018-02-11 23:54:18 -0600

    start 3.2 beta release; major code rewrite for #5

--- sample.c
+++ sample.c
@@ -1,2 +1,5 @@
+#################
+/*	sample.c	3.2.80	2018-02-11_23:53:12_CST uadmin six-rpi3b.cptx86.com 1.6-38-g874ab44 */
+/*	start 3.2 beta release; major code rewrite for #5 */
 #	sample.c	3.1.75	2018-02-11_21:41:21_CST uadmin six-rpi3b.cptx86.com 1.6-33-gd5d2018 
 #	testing sample.c with wrong password for github BradleyA 
