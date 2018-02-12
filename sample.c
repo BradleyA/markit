6d8266e9e308280ab83728f7f78103de250686d2
BradleyA <allen.bradley@ymail.com>
2018-02-12 00:06:45 -0600

    testing x.c x.go x for #5

--- sample.c
+++ sample.c
@@ -1,4 +1,7 @@
 #################
+/* 	sample.c	3.2.82	2018-02-12_00:05:49_CST uadmin six-rpi3b.cptx86.com 3.2-1-g2dafdfa  */
+/* 	testing x.c x.go x for #5  */
+
 /*	sample.c	3.2.80	2018-02-11_23:53:12_CST uadmin six-rpi3b.cptx86.com 1.6-38-g874ab44 */
 /*	start 3.2 beta release; major code rewrite for #5 */
 #	sample.c	3.1.75	2018-02-11_21:41:21_CST uadmin six-rpi3b.cptx86.com 1.6-33-gd5d2018 
---
+++ sample.go
@@ -0,0 +1,2 @@
+// 	sample.go	3.2.82	2018-02-12_00:05:49_CST uadmin six-rpi3b.cptx86.com 3.2-1-g2dafdfa 
+// 	testing x.c x.go x for #5 
