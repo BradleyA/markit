afa94a3979de2a9f7e83104c79fcc678dd00c610
BradleyA <allen.bradley@ymail.com>
2018-02-12 14:32:30 -0600

    beta 3.3 testing sample.go sample.c sample.xml sample.scala sample.htm sample.rb

--- sample.go
+++ sample.go
@@ -1,2 +1,7 @@
+#########################3333
+// 	sample.go	3.3.86	2018-02-12_14:31:08_CST uadmin six-rpi3b.cptx86.com 3.3 
+// 	beta 3.3 testing sample.go sample.c sample.xml sample.scala sample.htm sample.rb 
+
+
 // 	sample.go	3.2.82	2018-02-12_00:05:49_CST uadmin six-rpi3b.cptx86.com 3.2-1-g2dafdfa 
 // 	testing x.c x.go x for #5 
