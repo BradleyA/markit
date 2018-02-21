8b48eee5bf2c6a5d2616ee710068cda5f4f10b1e
BradleyA <allen.bradley@ymail.com>
2018-02-21 09:36:00 -0600

    typo in sample.sh test file

--- testfiles/sample.sh
+++ testfiles/sample.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# 	testfiles/sample.sh	3.8.133	2018-02-21_09:35:17_CST uadmin six-rpi3b.cptx86.com 3.7-2-g3ac7e20 
+# 	   typo in sample.sh test file 
 # 	testfiles/sample.sh	3.5.126	2018-02-18_21:33:53_CST uadmin six-rpi3b.cptx86.com 3.4-23-g1090ede 
 # 	   New release, all issues closed and tested, ready for production fix #12 
-#
-#
+###
 echo "Hello World"
