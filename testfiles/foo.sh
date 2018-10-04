071939545717099e8a01435a11acf98734473368
Bradley Allen <allen.bradley@ymail.com>
2018-10-04 17:11:50 -0500

    second test

--- testfiles/foo.sh
+++ testfiles/foo.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# 	foo.sh  3.83.233  2018-10-04_17:11:36_CDT  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.82  
+# 	   second test 
 # 	foo.sh  3.82.232  2018-10-04_16:58:11_CDT  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.81  
 # 	   this is a test 
 #  this is a test for markit on .sh file
+#  another update
