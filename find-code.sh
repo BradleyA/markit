eeb42329f11546dba3e013da64697c9fd9823402
Bradley Allen <allen.bradley@ymail.com>
2018-10-05 14:56:16 -0500

    begin find-code.sh Change echo or print DEBUG INFO WARNING ERROR #42

--- find-code.sh
+++ find-code.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+# 	find-code.sh  3.85.235  2018-10-05_14:56:04_CDT  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.84  
+# 	   begin find-code.sh Change echo or print DEBUG INFO WARNING ERROR #42 
 # 	find-code.sh  3.74.223  2018-09-04_21:45:40_CDT  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.73  
 # 	   redirect stderr to /dev/null to stop permission denied close #34 
 # 	find-code.sh  3.71.220  2018-09-04_17:12:40_CDT  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.70  
 # 	   move find-code.sh from user-work-files to markit 
-###
-###
-DEBUG=0                 # 0 = debug off, 1 = debug on
+#
+###	find-code.sh - Search systems from clones from repositories
+DEBUG=1                 # 0 = debug off, 1 = debug on
 #	set -x
 #	set -v
 BOLD=$(tput bold)
@@ -14,7 +16,7 @@ NORMAL=$(tput sgr0)
 display_help() {
 echo -e "\n${NORMAL}${0} - Search systems from clones from repositories"
 echo -e "\nUSAGE\n   ${0} [<path>/<HOSTFILE>]"
-echo    "   ${0} [--help | -help | help | -h | h | -? | ?]"
+echo    "   ${0} [--help | -help | help | -h | h | -?]"
 echo    "   ${0} [--version | -version | -v]"
 echo -e "\nDESCRIPTION\nThis script runs a command xxxxx ->"
 echo    "xxx"
@@ -22,18 +24,48 @@ echo -e "\nOPTIONS\n"
 echo    "   HOSTFILE   File with hostnames, default /usr/local/data/us-tx-cluster-1/SYSTEMS"
 echo -e "\nDOCUMENTATION\nhttps://github.com/BradleyA/user-work-files"
 echo -e "\nEXAMPLES\n   ${0}\n\n   what does it do\n"
+#       After displaying help in english check for other languages
 if ! [ "${LANG}" == "en_US.UTF-8" ] ; then
-	echo -e "${NORMAL}${0} ${LINENO} [${BOLD}WARNING${NORMAL}]:	Your language, ${LANG}, is not supported.\n\tWould you like to help?\n"	1>&2
+        get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[WARN]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  Your language, ${LANG}, is not supported, Would you like to help translate?" 1>&2
+#       elif [ "${LANG}" == "fr_CA.UTF-8" ] ; then
+#               get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[WARN]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  Display help in ${LANG}" 1>&2
+#       else
+#               get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[WARN]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  Your language, ${LANG}, is not supported.\tWould you like to translate?" 1>&2
 fi
 }
-if [ "$1" == "--help" ] || [ "$1" == "-help" ] || [ "$1" == "help" ] || [ "$1" == "-h" ] || [ "$1" == "h" ] || [ "$1" == "-?" ] || [ "$1" == "?" ] ; then
-	display_help
-	exit 0
+
+#       Date and time function
+get_date_stamp() {
+DATE_STAMP=`date +%Y-%m-%d-%H-%M-%S-%Z`
+}
+
+#  Fully qualified domain name FQDN hostname
+LOCALHOST=`hostname -f`
+
+#  Version
+SCRIPT_NAME=`head -2 ${0} | awk {'printf$2'}`
+SCRIPT_VERSION=`head -2 ${0} | awk {'printf$3'}`
+
+#       UID and GID
+USER_ID=`id -u`
+GROUP_ID=`id -g`
+
+#       Default help and version arguments
+if [ "$1" == "--help" ] || [ "$1" == "-help" ] || [ "$1" == "help" ] || [ "$1" == "-h" ] || [ "$1" == "h" ] || [ "$1" == "-?" ] ; then
+        display_help
+        exit 0
 fi
 if [ "$1" == "--version" ] || [ "$1" == "-version" ] || [ "$1" == "version" ] || [ "$1" == "-v" ] ; then
-	head -2 ${0} | awk {'print$2"\t"$3'}
-	exit 0
+        echo "${SCRIPT_NAME} ${SCRIPT_VERSION}"
+        exit 0
 fi
+
+#       INFO
+get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[INFO]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  Begin" 1>&2
+
+#       DEBUG
+if [ "${DEBUG}" == "1" ] ; then get_date_stamp ; echo -e "${NORMAL}${DATE_STAMP} ${0} ${SCRIPT_VERSION} ${LINENO} ${BOLD}[DEBUG]${NORMAL}  ${LOCALHOST}  ${USER}  ${USER_ID} ${GROUP_ID}  Name_of_command >${0}< Name_of_arg1 >${1}<" 1>&2 ; fi
+
 ###
 HOSTFILE=${1:-"/usr/local/data/us-tx-cluster-1/SYSTEMS"}
 LOCALHOST=`hostname -f`
