bf30680c4b1f9150abdbbcb225b54a4409314d33
Bradley Allen <allen.bradley@ymail.com>
2018-10-05 22:08:19 -0500

    update for display_help #43

--- find-code.sh
+++ find-code.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 	find-code.sh  3.88.238  2018-10-05_22:08:01_CDT  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.87  
+# 	   update for display_help #43 
 # 	find-code.sh  3.87.237  2018-10-05_21:42:15_CDT  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.86  
 # 	   Change echo or print DEBUG INFO WARNING ERROR close #42 
 #
@@ -10,12 +12,33 @@ BOLD=$(tput bold)
 NORMAL=$(tput sgr0)
 ###
 display_help() {
-echo -e "\n${NORMAL}${0} - Search systems from clones from repositories"
+echo -e "\n${NORMAL}${0} - Search systems for .git repositories"
 echo -e "\nUSAGE\n   ${0} [<path>/<HOSTFILE>]"
 echo    "   ${0} [--help | -help | help | -h | h | -?]"
 echo    "   ${0} [--version | -version | -v]"
-echo -e "\nDESCRIPTION\nThis script runs a command xxxxx ->"
-echo    "xxx"
+echo -e "\nDESCRIPTION\nSearch systems for .git repositories."
+echo    "/usr/local/data/us-tx-cluster-1/SYSTEMS"
+
+echo -e "\nThis script reads /usr/local/data/us-tx-cluster-1/SYSTEMS file for hosts."
+echo    "The hosts are one FQDN or IP address per line for all hosts in a cluster."
+echo    "Lines in SYSTEMS file that begin with a # are comments.  The SYSTEMS file is"
+echo    "used by Linux-admin/cluster-command/cluster-command.sh, markit/find-code.sh,"
+echo    "pi-display/create-message/create-message.sh, and other scripts.  A different"
+echo    "SYSTEMS file can be entered on the command line or environment variable."
+echo -e "\nTo avoid many login prompts for each host in a cluster, enter the following:"
+echo    "${BOLD}ssh-copy-id uadmin@<host-name>${NORMAL} to each host in the SYSTEMS file."
+echo -e "\nEnvironment Variables"
+echo    "If using the bash shell, enter; export CLUSTER='us-west1' on the command"
+echo    "line to set the CLUSTER environment variable to 'us-west1'.  Use the command,"
+echo    "unset CLUSTER to remove the exported information from the CLUSTER environment"
+echo    "variable.  To set an environment variable to be defined at login, add it to"
+echo    "~/.bashrc file or you can modify this script with your default location for"
+echo    "CLUSTER, DATA_DIR, MESSAGE_FILE, and SYSTEMS_FILE.  You are on your own"
+echo    "defining environment variables if you are using other shells."
+echo    "   CLUSTER       (default us-tx-cluster-1/)"
+echo    "   DATA_DIR      (default /usr/local/data/)"
+echo    "   SYSTEMS_FILE  (default SYSTEMS)"
+
 echo -e "\nOPTIONS\n"
 echo    "   HOSTFILE   File with hostnames, default /usr/local/data/us-tx-cluster-1/SYSTEMS"
 echo -e "\nDOCUMENTATION\nhttps://github.com/BradleyA/user-work-files"
