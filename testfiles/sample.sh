e29c00140518360155fea03ac8821f0478ed835c
Bradley Allen <allen.bradley@ymail.com>
2018-10-07 13:31:04 -0500

    test markit for sample.sh

--- testfiles/sample.sh
+++ testfiles/sample.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# 	sample.sh  3.96.247  2018-10-07T08:59:51-05:00 (CDT)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.95-1-g07806e2  
-# 	   update comments 
+# 	testfiles/sample.sh  3.108.260  2018-10-07T13:31:04-05:00 (CDT)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.107  
+# 	   test markit for sample.sh 
 #
 ###
 echo "Hello World"
