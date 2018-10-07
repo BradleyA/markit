da3c54434f7efee5b760553d4364cc08820d42d4
Bradley Allen <allen.bradley@ymail.com>
2018-10-07 11:43:47 -0500

    markit Change echo or print DEBUG INFO WARNING ERROR #40

--- testfiles/foo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# 	foo.sh  3.84.234  2018-10-04_17:12:32_CDT  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.83  
-# 	   test three 
-# 	foo.sh  3.83.233  2018-10-04_17:11:36_CDT  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.82  
-###
-# 	   this is a test 
-#  this is a test for markit on .sh file
-#  another update
-#  third update to file
