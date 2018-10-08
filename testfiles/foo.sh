36f409731f7a8660c5f75328556f4f926267e60f
Bradley Allen <allen.bradley@ymail.com>
2018-10-07 22:18:03 -0500

    this is a test

---
+++ testfiles/foo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# 	foo.sh  3.109.262  2018-10-07T22:18:03-05:00 (CDT)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.108-1-ga09d833  
+# 	   this is a test 
+#
+###	
+#	this is a test
