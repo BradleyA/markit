2e733c2bebeef9054b141c636e18af4e2cc1d345
Bradley Allen <allen.bradley@ymail.com>
2018-10-07 22:24:43 -0500

    test three and data file

---
+++ testfiles/foobar.sh
@@ -0,0 +1,5 @@
+#
+# 	foobar.sh  3.111.264  2018-10-07T22:24:43-05:00 (CDT)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.110  
+# 	   test three and data file 
+#
+### data file for foo.sh
