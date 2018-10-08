b4df7b31f521bfe7cd65f1ea3141abe796a343f8
Bradley Allen <allen.bradley@ymail.com>
2018-10-07 22:20:50 -0500

    second test

--- testfiles/foo.sh
+++ testfiles/foo.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# 	foo.sh  3.110.263  2018-10-07T22:20:50-05:00 (CDT)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.109  
+# 	   second test 
 # 	foo.sh  3.109.262  2018-10-07T22:18:03-05:00 (CDT)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.108-1-ga09d833  
 # 	   this is a test 
 #
 ###	
 #	this is a test
+#	this is second test
