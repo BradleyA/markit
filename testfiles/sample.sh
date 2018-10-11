936749408f5b8a8d775239e55ff893267316e00a
Bradley Allen <allen.bradley@ymail.com>
2018-10-11 18:54:43 -0500

    testing network at Linux meeting

--- testfiles/sample.sh
+++ testfiles/sample.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# 	sample.sh  3.115.268  2018-10-11T18:54:42-05:00 (CDT)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.114  
+# 	   testing network at Linux meeting 
 # 	sample.sh  3.114.267  2018-10-11T11:24:19-05:00 (CDT)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.113  
 # 	   update example file after test 
 #
 ###
 echo "Hello World"
+#	test network
