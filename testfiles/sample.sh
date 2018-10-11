d939057b84d4c591eb28d5572da59e53f7446220
Bradley Allen <allen.bradley@ymail.com>
2018-10-11 11:19:27 -0500

    update file

--- testfiles/sample.sh
+++ testfiles/sample.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# 	sample.sh  3.113.266  2018-10-11T11:19:27-05:00 (CDT)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.112  
+# 	   update file 
 # 	testfiles/sample.sh  3.108.260  2018-10-07T13:31:04-05:00 (CDT)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.107  
 # 	   test markit for sample.sh 
 #
 ###
 echo "Hello World"
+echo "Linux group"
