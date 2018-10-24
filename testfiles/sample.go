eb6e83911de3d4de7b2acbd65d105ea9102bd109
Bradley Allen <allen.bradley@ymail.com>
2018-10-23 21:23:22 -0500

    added example on how to run go program

--- testfiles/sample.go
+++ testfiles/sample.go
@@ -1,7 +1,10 @@
 // 	
+// 	testfiles/sample.go  3.125.280  2018-10-23T21:23:22.550260-05:00 (CDT)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.125  
+// 	   added example on how to run go program 
 // 	sample.go  3.38.179  2018-08-04_13:18:57_CDT  https://github.com/BradleyA/markit  uadmin  three-rpi3b.cptx86.com 3.37  
 // 	   New release not backward compatible with markchaeck because of formatting 
 // 	
+//	$ git add foo.sh
 package main
 
 import "fmt"
