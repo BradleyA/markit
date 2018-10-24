d88d3c5a23d9a8707c91087cd8fe868bba2e02ec
Bradley Allen <allen.bradley@ymail.com>
2018-10-23 21:17:04 -0500

    this is a py and sh test

---
+++ testfiles/bob.py
@@ -0,0 +1,3 @@
+thisw is a py test
+# 	testfiles/bob.py  3.125.279  2018-10-23T21:17:04.279684-05:00 (CDT)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.124-1-gfb07ea8  
+# 	   this is a py and sh test 
