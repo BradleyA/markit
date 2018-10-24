b772e503bdfebec6977e894bc71970078820f1aa
Bradley Allen <allen.bradley@ymail.com>
2018-10-23 21:30:27 -0500

    updated comments about how to run a.out

--- testfiles/sample.c
+++ testfiles/sample.c
@@ -1,7 +1,11 @@
 /* 	*/
+/* 	testfiles/sample.c  3.125.281  2018-10-23T21:30:27.421207-05:00 (CDT)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.125-1-geb6e839   */
+/* 	   updated comments about how to run a.out  */
 /* 	sample.c  3.38.179  2018-08-04_13:18:57_CDT  https://github.com/BradleyA/markit  uadmin  three-rpi3b.cptx86.com 3.37   */
 /* 	   New release not backward compatible with markchaeck because of formatting  */
 /* 	*/
+/* 	$ gcc sample.c		*/
+/* 	$ ./a.out		*/
 #include <stdio.h>
 int main()
 {
