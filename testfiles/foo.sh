cf6cfeb5ef990cccfd39f9b9cfb85006d5d9c309
Bradley Allen <allen.bradley@ymail.com>
2018-10-23 21:09:25 -0500

    this is a test for markit

---
+++ testfiles/foo.sh
@@ -0,0 +1,3 @@
+this is a test
+# 	testfiles/foo.sh  3.124.277  2018-10-23T21:09:25.764632-05:00 (CDT)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.123  
+# 	   this is a test for markit 
