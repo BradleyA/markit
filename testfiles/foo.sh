fb07ea8cd322f9af6b49c3bc0576877d030aabe8
Bradley Allen <allen.bradley@ymail.com>
2018-10-23 21:14:06 -0500

    this is a test

--- testfiles/foo.sh
+++ testfiles/foo.sh
@@ -1,3 +1,6 @@
-this is a test
+#	this is a test
+# 	testfiles/foo.sh  3.124.278  2018-10-23T21:14:06.795306-05:00 (CDT)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.124  
+# 	   this is a test 
 # 	testfiles/foo.sh  3.124.277  2018-10-23T21:09:25.764632-05:00 (CDT)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.123  
 # 	   this is a test for markit 
+code goes here
