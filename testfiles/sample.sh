265e0b9d1f62c58e81b1b39e857aec2e30424238
Bradley Allen <allen.bradley@ymail.com>
2018-11-08 20:12:02 -0600

    test

--- testfiles/sample.sh
+++ testfiles/sample.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 	testfiles/sample.sh  3.131.287  2018-11-08T20:12:02.249564-06:00 (CST)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.130  
+# 	   test 
 # 	testfiles/sample.sh  3.130.286  2018-11-08T18:59:51.665183-06:00 (CST)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.129  
 # 	   test for meetup 
 # 	sample.sh  3.114.267  2018-10-11T11:24:19-05:00 (CDT)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.113  
@@ -7,3 +9,4 @@
 ###
 echo "Hello World"
 #	test network
+# hello
