49c4dff87517c900b1711b2f8097822f9edf837b
Bradley Allen <allen.bradley@ymail.com>
2019-02-05 14:16:01 -0600

    test with no change to version number

--- testfiles/sample.sh
+++ testfiles/sample.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 	testfiles/sample.sh  3.156.321  2019-02-05T14:16:01.037344-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.156  
+# 	   test with no change to version number 
 # 	testfiles/sample.sh  3.156.320  2019-02-05T13:39:47.315034-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.155-1-gbe2f1f3  
 # 	   testing gitlog output from this test 
 # 	testfiles/sample.sh  3.155.319  2019-02-05T13:02:22.031043-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.155  
@@ -12,4 +14,3 @@
 #
 ###
 echo "Hello World"
-
