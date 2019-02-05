c4ab030ce8a51b9d56627c824d92e5bb34e6867a
Bradley Allen <allen.bradley@ymail.com>
2019-02-05 16:07:30 -0600

    completed testing of markit

--- testfiles/sample.sh
+++ testfiles/sample.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
+# 	testfiles/sample.sh  3.157.322  2019-02-05T16:07:30.573810-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.156-1-g49c4dff  
+# 	   completed testing of markit 
 # 	testfiles/sample.sh  3.156.321  2019-02-05T14:16:01.037344-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.156  
 # 	   test with no change to version number 
-# 	testfiles/sample.sh  3.156.320  2019-02-05T13:39:47.315034-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.155-1-gbe2f1f3  
-# 	   testing gitlog output from this test 
 # 	testfiles/sample.sh  3.155.319  2019-02-05T13:02:22.031043-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.155  
 # 	   testing patch level Semantic Versioning 
-# 	testfiles/sample.sh  3.131.287  2018-11-08T20:12:02.249564-06:00 (CST)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.130  
-# 	   test 
 # 	testfiles/sample.sh  3.130.286  2018-11-08T18:59:51.665183-06:00 (CST)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.129  
 # 	   test for meetup 
 # 	sample.sh  3.114.267  2018-10-11T11:24:19-05:00 (CDT)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.113  
