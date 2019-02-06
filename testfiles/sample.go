28b8b93bf8760eee3d979a8da31cdcd1e7768652
Bradley Allen <allen.bradley@ymail.com>
2019-02-06 10:31:06 -0600

    add build comment to sample.go

--- testfiles/sample.go
+++ testfiles/sample.go
@@ -1,15 +1,22 @@
 // 	
+// 	testfiles/sample.go  3.159.324  2019-02-06T10:31:06.641225-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.158  
+// 	   add build comment to sample.go 
 // 	testfiles/sample.go  3.125.280  2018-10-23T21:23:22.550260-05:00 (CDT)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.125  
 // 	   added example on how to run go program 
 // 	sample.go  3.38.179  2018-08-04_13:18:57_CDT  https://github.com/BradleyA/markit  uadmin  three-rpi3b.cptx86.com 3.37  
 // 	   New release not backward compatible with markchaeck because of formatting 
 // 	
-//	$ git add foo.sh
+//	$ go run sample.go
+//	hello world
+//	$ go build sample.go
+//	$ ./sample
+//	hello world in go
+//	
 package main
 
 import "fmt"
 
 func main() {
-    fmt.Println("hello world")
+    fmt.Println("hello world in go")
 }
 
