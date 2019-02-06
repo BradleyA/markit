feefe44bfcf210e5be5e256979b1fbea15dfe9e3
Bradley Allen <allen.bradley@ymail.com>
2019-02-06 17:05:27 -0600

    update user help message for untracked files

---
+++ testfiles/foo.pl
@@ -0,0 +1,7 @@
+
+# 	testfiles/foo.pl  3.178.346  2019-02-06T17:05:27.874631-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.177  
+# 	   update user help message for untracked files 
+
+
+
+testing
