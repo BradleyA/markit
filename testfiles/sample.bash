dbbb23f2839862d76cf9ac90f43ffef3832cb083
Bradley Allen <allen.bradley@ymail.com>
2019-02-06 11:34:55 -0600

    add support for tcsh csh ksh etc #59 testing

--- testfiles/sample.bash
+++ testfiles/sample.bash
@@ -1,8 +1,6 @@
 #!/bin/bash
-# 	testfiles/sample.bash  3.166.331  2019-02-06T11:34:02.684353-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.165  
+# 	testfiles/sample.bash  3.167.332  2019-02-06T11:34:55.480714-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.166  
 # 	   add support for tcsh csh ksh etc #59 testing 
-# 	testfiles/sample.sh  3.161.326  2019-02-06T11:08:27.868358-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.160  
-# 	   add support for tcsh csh ksh etc close #59 
 # 	testfiles/sample.sh  3.155.319  2019-02-05T13:02:22.031043-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.155  
 # 	   testing patch level Semantic Versioning 
 # 	testfiles/sample.sh  3.130.286  2018-11-08T18:59:51.665183-06:00 (CST)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.129  
