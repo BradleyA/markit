1638d802de4560188073ad59a181889571289688
Bradley Allen <allen.bradley@ymail.com>
2019-02-06 11:16:29 -0600

    add support for tcsh csh ksh etc #59 testing

--- testfiles/sample.sh
+++ testfiles/sample.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# 	testfiles/sample.sh  3.160.325  2019-02-06T10:40:57.200368-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.159  
-# 	   added comment to run perl script 
-# 	testfiles/sample.sh  3.157.322  2019-02-05T16:07:30.573810-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.156-1-g49c4dff  
-# 	   completed testing of markit 
-# 	testfiles/sample.sh  3.156.321  2019-02-05T14:16:01.037344-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.156  
-# 	   test with no change to version number 
+# 	testfiles/sample.sh  3.161.326  2019-02-06T11:16:29.644324-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.160  
+# 	   add support for tcsh csh ksh etc #59 testing 
+# 	testfiles/sample.sh  3.161.326  2019-02-06T11:15:57.951161-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.160  
+# 	   add support for tcsh csh ksh etc #59 testing 
+# 	testfiles/sample.sh  3.161.326  2019-02-06T11:11:52.610122-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.160  
+# 	   add support for tcsh csh ksh etc #59 testing 
+# 	testfiles/sample.sh  3.161.326  2019-02-06T11:08:27.868358-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.160  
+# 	   add support for tcsh csh ksh etc close #59 
 # 	testfiles/sample.sh  3.155.319  2019-02-05T13:02:22.031043-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.155  
 # 	   testing patch level Semantic Versioning 
 # 	testfiles/sample.sh  3.130.286  2018-11-08T18:59:51.665183-06:00 (CST)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.129  
@@ -12,5 +14,6 @@
 # 	sample.sh  3.114.267  2018-10-11T11:24:19-05:00 (CDT)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.113  
 # 	   update example file after test 
 #
+#	./sample.sh
 ###
 echo "Hello World"
