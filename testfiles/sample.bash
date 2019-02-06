6b8787921911a6bc73b56ab5e84ed1c55c2cff20
Bradley Allen <allen.bradley@ymail.com>
2019-02-06 11:32:19 -0600

    add support for tcsh csh ksh etc #59 testing

---
+++ testfiles/sample.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+# 	testfiles/sample.bash  3.165.330  2019-02-06T11:32:19.709405-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.164  
+# 	   add support for tcsh csh ksh etc #59 testing 
+# 	testfiles/sample.sh  3.161.326  2019-02-06T11:16:29.644324-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.160  
+# 	   add support for tcsh csh ksh etc #59 testing 
+# 	testfiles/sample.sh  3.161.326  2019-02-06T11:15:57.951161-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.160  
+# 	   add support for tcsh csh ksh etc #59 testing 
+# 	testfiles/sample.sh  3.161.326  2019-02-06T11:11:52.610122-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.160  
+# 	   add support for tcsh csh ksh etc #59 testing 
+# 	testfiles/sample.sh  3.161.326  2019-02-06T11:08:27.868358-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.160  
+# 	   add support for tcsh csh ksh etc close #59 
+# 	testfiles/sample.sh  3.155.319  2019-02-05T13:02:22.031043-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.155  
+# 	   testing patch level Semantic Versioning 
+# 	testfiles/sample.sh  3.130.286  2018-11-08T18:59:51.665183-06:00 (CST)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.129  
+# 	   test for meetup 
+# 	sample.sh  3.114.267  2018-10-11T11:24:19-05:00 (CDT)  https://github.com/BradleyA/markit  bradley  zero.cptx86.com 3.113  
+# 	   update example file after test 
+#
+#	./sample.sh
+###
+echo "Hello World"
