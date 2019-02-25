f540742c3aa1d122f70b3d30404b9d9ed89c2dc1
Bradley Allen <allen.bradley@ymail.com>
2019-02-25 13:30:40 -0600

    modified .git/config to push to github, gitlab, and bitbucket

--- push-two-repository-test.sh
+++ push-two-repository-test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# 	push-two-repository-test.sh  3.207.498  2019-02-25T13:30:40.766777-06:00 (CST)  https://github.com/BradleyA/markit.git  uadmin  six-rpi3b.cptx86.com 3.206-3-g31765fb  
+# 	   modified .git/config to push to github, gitlab, and bitbucket 
 
 ###
 #	push to github and bitbucket
+#	push to github, bitbucket, and gitlab
