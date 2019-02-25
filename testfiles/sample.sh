ad689263bbd78b43dc4aa321260c78e383e36fce
Bradley Allen <allen.bradley@ymail.com>
2019-02-25 14:07:28 -0600

    test git push using markit to github, gitlab, and bitbucket close #63

--- testfiles/sample.sh
+++ testfiles/sample.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# 	testfiles/sample.sh  3.209.502  2019-02-25T14:07:27.899183-06:00 (CST)  https://github.com/BradleyA/markit.git  uadmin  six-rpi3b.cptx86.com 3.208-2-g334d4ff  
+# 	   test git push using markit to github, gitlab, and bitbucket close #63 
 # 	testfiles/sample.sh  3.197.368  2019-02-08T20:33:53.600661-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.196-1-g46f4c0d  
 # 	   test markit for sample.sh 
 #
-#	./sample.sh
-###
+### ./sample.sh
 echo "Hello World"
