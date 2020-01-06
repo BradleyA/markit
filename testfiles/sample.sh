bc86f54b6837b6d798b3315bd7427cea79382559
Bradley Allen <allen.bradley@ymail.com>
2020-01-06 13:08:29 -0600

    testfiles/sample.sh   testing changes to markit

--- testfiles/sample.sh
+++ testfiles/sample.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# 	testfiles/sample.sh  3.240.556  2020-01-06T13:01:45.468761-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  five-rpi3b.cptx86.com 3.239  
-# 	   markit   testing changes 
+# 	testfiles/sample.sh  3.241.557  2020-01-06T13:08:29.614441-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  five-rpi3b.cptx86.com 3.240  
+# 	   testfiles/sample.sh   testing changes to markit 
 # 	testfiles/sample.sh  3.209.502  2019-02-25T14:07:27.899183-06:00 (CST)  https://github.com/BradleyA/markit.git  uadmin  six-rpi3b.cptx86.com 3.208-2-g334d4ff  
 # 	   test git push using markit to github, gitlab, and bitbucket close #63 
 ### ./sample.sh
