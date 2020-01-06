1d38bd49ec63454fc71afd3778cd492cb07df682
Bradley Allen <allen.bradley@ymail.com>
2020-01-06 11:28:43 -0600

    test g

--- testfiles/sample.sh
+++ testfiles/sample.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 	testfiles/sample.sh  3.234.548  2020-01-06T11:28:43.070263-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  five-rpi3b.cptx86.com 3.233  
+# 	   test g 
 # 	testfiles/sample.sh  3.226.542    https://github.com/BradleyA/markit  uadmin  five-rpi3b.cptx86.com 3.225  
 # 	   testfiles/sample.sh   testing markit changes 
 # 	testfiles/sample.sh  3.209.502  2019-02-25T14:07:27.899183-06:00 (CST)  https://github.com/BradleyA/markit.git  uadmin  six-rpi3b.cptx86.com 3.208-2-g334d4ff  
@@ -7,3 +9,4 @@
 # 	   test markit for sample.sh 
 ### ./sample.sh
 echo "Hello World"
+echo "testing markit changes"
