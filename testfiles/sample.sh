ef33d26b23c4aac590dcf80186944b94429fcbda
Bradley Allen <allen.bradley@ymail.com>
2020-09-10 16:46:02 -0500

    testfiles/sample.sh -->   testing markit after editing testfiles/sample.sh

--- testfiles/sample.sh
+++ testfiles/sample.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# 	testfiles/sample.sh  4.1.5.807  2020-09-10T16:43:51.470271-05:00 (CDT)  https://github.com/BradleyA/markit.git  master  uadmin  five-rpi3b.cptx86.com 4.1.4-3-g435936c  
+# 	testfiles/sample.sh  4.1.6.813  2020-09-10T16:46:02.111314-05:00 (CDT)  https://github.com/BradleyA/markit.git  master  uadmin  five-rpi3b.cptx86.com 4.1.5-5-g1331dac  
 # 	   testfiles/sample.sh -->   testing markit after editing testfiles/sample.sh  
 # 	testfiles/sample.sh  3.209.502  2019-02-25T14:07:27.899183-06:00 (CST)  https://github.com/BradleyA/markit.git  uadmin  six-rpi3b.cptx86.com 3.208-2-g334d4ff  
 # 	   test git push using markit to github, gitlab, and bitbucket close #63 
 ### ./sample.sh
-
 echo "Hello World"
