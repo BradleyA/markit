6df6c8b30d8e64ab58145f1d8500c47cd7cb3ae6
Bradley Allen <BradleyA@users.noreply.github.com>
2020-09-10 21:28:45 -0500

    Update find-code.sh

--- find-code.sh
+++ find-code.sh
@@ -99,11 +99,11 @@ echo -e "\tOn-line command usage\n"                                             
 echo    "   --version, -version, -v"                                                      # 0.3.579
 echo -e "\tOn-line command version\n"                                                  # 3.572
 #
-echo    "   -c, --cluster, -c=, --cluster=<CLUSTER>"
+echo    "   --cluster, -c, --cluster=<CLUSTER>", -c=
 echo -e "\tCluster directory name (default '${DEFAULT_CLUSTER}')\n"
-echo    "   -d, --datadir, -d=, --datadir=<DATA_DIR>"
+echo    "   --datadir, -d, --datadir=<DATA_DIR>", -d=
 echo -e "\tPath to cluster data directory (default '${DEFAULT_DATA_DIR}')\n"
-echo    "   -s, --systems, -s=, --systems=<SYSTEMS_FILE>"
+echo    "   --systems, -s, --systems=<SYSTEMS_FILE>", -s=
 echo -e "\tName of systems file (default ${DEFAULT_SYSTEMS_FILE})"
 
 ###  Production standard 6.3.547  Architecture tree
