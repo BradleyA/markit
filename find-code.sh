2094529156d01d11836cd168a79588228b4636a6
Bradley Allen <allen.bradley@ymail.com>
2020-10-21 19:28:02 -0500

    find-code.sh -->   testing FVT-option-cluster-001

--- find-code.sh
+++ find-code.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# 	find-code.sh  4.1.23.910  2020-10-21T14:28:05.073387-05:00 (CDT)  https://github.com/BradleyA/markit  master  uadmin  five-rpi3b.cptx86.com 4.1.22-1-g4aba95a  
+# 	find-code.sh  4.1.24.911  2020-10-21T19:28:01.906915-05:00 (CDT)  https://github.com/BradleyA/markit  master  uadmin  five-rpi3b.cptx86.com 4.1.23  
+# 	   find-code.sh -->   testing FVT-option-cluster-001  
+# 	find-code.sh  4.1.23.910  2020-10-21T14:28:05.073387-05:00 (CDT)  https://github.com/BradleyA/markit  master  uadmin  five-rpi3b.cptx86.com 4.1.22-1-g4aba95a 
 # 	   find-code.sh -->   correct options -u incidnet  
 # 	find-code.sh  4.1.22.908  2020-10-21T14:20:37.170990-05:00 (CDT)  https://github.com/BradleyA/markit  master  uadmin  five-rpi3b.cptx86.com 4.1.21  
 # 	   find-code.sh -->   begin testing options  
