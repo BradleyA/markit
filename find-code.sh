ffccff3f5dd45b1e2edefe9b287ce891934db26f
Bradley Allen <allen.bradley@ymail.com>
2020-11-12 21:41:14 -0600

    find-code.sh -->   testing

--- find-code.sh
+++ find-code.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 	find-code.sh  4.1.111.1033  2020-11-12T13:41:34.811723-06:00 (CST)  https://github.com/BradleyA/markit  master  uadmin  five-rpi3b.cptx86.com 4.1.110  
+# 	find-code.sh  4.1.115.1037  2020-11-12T21:41:13.986427-06:00 (CST)  https://github.com/BradleyA/markit  master  uadmin  five-rpi3b.cptx86.com 4.1.114  
 # 	   find-code.sh -->   testing  
 # 	find-code.sh  4.1.108.1030  2020-11-12T13:13:54.908899-06:00 (CST)  https://github.com/BradleyA/markit  master  uadmin  five-rpi3b.cptx86.com 4.1.107 
 # 	   find-code.sh -->   Production standard 9.3.606 Parse CLI options and arguments  
