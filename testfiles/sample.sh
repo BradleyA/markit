b0bf2973bb9192fca51cc1c2c729086122cffb3e
Bradley Allen <allen.bradley@ymail.com>
2021-01-02 14:55:24 -0600

    testfiles/sample.sh -->   recording animatation after editing testfile/sample.sh

--- testfiles/sample.sh
+++ testfiles/sample.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# 	testfiles/sample.sh  4.2.2.1119  2021-01-02T14:55:24.774143-06:00 (CST)  https://github.com/BradleyA/markit  master  uthree  three.cptx86.com 4.2.1-4-gf1e2ffd  
+# 	   testfiles/sample.sh -->   recording animatation after editing testfile/sample.sh  
 # 	testfiles/sample.sh  4.1.6.813  2020-09-10T16:46:02.111314-05:00 (CDT)  https://github.com/BradleyA/markit.git  master  uadmin  five-rpi3b.cptx86.com 4.1.5-5-g1331dac  
 # 	   testfiles/sample.sh -->   testing markit after editing testfiles/sample.sh  
 # 	testfiles/sample.sh  3.209.502  2019-02-25T14:07:27.899183-06:00 (CST)  https://github.com/BradleyA/markit.git  uadmin  six-rpi3b.cptx86.com 3.208-2-g334d4ff  
 # 	   test git push using markit to github, gitlab, and bitbucket close #63 
 ### ./sample.sh
+
 echo "Hello World"
