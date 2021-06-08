acc89c0315e43cceefd42ef733a5c6b1a8874756
Bradley Allen <allen.bradley@ymail.com>
2021-06-08 13:31:04 -0500

    testfiles/sample.bash -->   testing patch level Semantic Versioning 4.2.9-beta.1

--- testfiles/sample.bash
+++ testfiles/sample.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 	testfiles/sample.bash  4.2.9-beta.1.1151  2021-06-08T13:31:04.697362-05:00 (CDT)  https://github.com/BradleyA/markit.git  master  uthree  three.cptx86.com 4.2.9-1-g687b283  
+# 	   testfiles/sample.bash -->   testing patch level Semantic Versioning 4.2.9-beta.1  
 # 	testfiles/sample.bash  3.167.332  2019-02-06T11:34:55.480714-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.166  
 # 	   add support for tcsh csh ksh etc #59 testing 
 # 	testfiles/sample.sh  3.155.319  2019-02-05T13:02:22.031043-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.155  
@@ -10,4 +12,5 @@
 #
 #	./sample.sh
 ###
+
 echo "Hello World"
